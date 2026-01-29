c3279911dea9f06aece6a20e3e524b406b9e6999
Rati Patil <patilratikant8@gmail.com>
2026-01-29 11:55:24 +0530

    Practice Questions

---
+++ Practice/Addition.py
@@ -0,0 +1,6 @@
+#Write a program to add two numbers provided by the user and display the result.
+
+a = int(input("Enter a first number:  "))
+b = int(input("Enter a second number: "))
+sum = a + b
+print("The sum of", a, "and", b, "is:", sum)
---
+++ Practice/Avg.py
@@ -0,0 +1,5 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+avarage = (a + b) / 2 
+print("The average of", a, "and", b, "is:", avarage)
