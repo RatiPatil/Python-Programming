0195027ea7c29312bb2c952e04638015ae0058f1
Rati Patil <patilratikant8@gmail.com>
2026-01-31 21:16:21 +0530

    StringSlicing

---
+++ Slicing.py
@@ -0,0 +1,18 @@
+name = "Ratikant"
+
+print(name[1])# Output: a
+print(name[3])# Output: i 
+print(name[0:4])# Output: Rati
+print(name[2:])# Output: tikant
+print(name[:5])# Output: Ratika
+print(name[-1])# Output: t
+print(name[-4:-1])# Output: kan
+print(name[-6:])# Output: tikant
+print(name[:-3])# Output: Ratika
+print(name[:])# Output: Ratikant
+print(name[::2])# Output: Rtkat
+print(name[1::2])# Output: ai n
+print(name[::-1])# Output: tnakitaR
+print(name[::3])# Output: Ria
+print(name[-1::-1])# Output: tnakitaR
+print(name[2:len(name)])# Output: tikant
