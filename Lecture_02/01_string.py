2761ab1da6d1f50f44466b75d471c9c32f884b11
Rati Patil <patilratikant8@gmail.com>
2026-02-02 08:18:03 +0530

    Lecture_02_StringConcepts

---
+++ Lecture_02/01_string.py
@@ -0,0 +1,23 @@
+name = "ratikant patil"
+name.upper()
+print( name.upper() )
+name.lower()
+print( name.lower() )
+name.count("a")
+print( name.count("a") )
+name.count("r")
+print( name.count("r") )
+
+name.endswith("l")
+print( name.endswith("l") )
+name.endswith("k")
+print( name.endswith("k") )
+name.startswith("r")
+print( name.startswith("r") )
+name.startswith("a")
+print( name.startswith("a") )
+
+
+name.capitalize()
+name.replace("ratikant", "Radhakrishna" )
+print( name.replace("ratikant", "Radhakrishna" ) )
